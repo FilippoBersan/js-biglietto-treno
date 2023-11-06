22eaa301920d9cdbe32614fe0297fe05fedeaea8
FilippoBersan <filippobersan99@gmail.com>
2023-11-06 16:39:30 +0100

    feat: javascript (istruzione condizionale bug)

--- main.js
+++ main.js
@@ -3,10 +3,39 @@
 // Calcolo del prezzo del biglieggto del treno
 
 // 1. mostro un prompt per il numero chilometri da percorrere
-// 2. mostro un prompt per l'età del passeggero
-// 3. il prezzo del biglietto sarà definito in base ai km 0.21
+const chilometri = parseInt(prompt('Inserisci chilometri da percorrere'));
 
-// 4. istruzione condizionale
+// 2. mostro un prompt per l'età del passeggero
+const eta = parseInt(prompt('Inserisci età del passeggero'));
+
+// 3. salvo in una variabile il numero di chilometri
+const numeroChilometri = chilometri;
+
+// 4. salvo in una variabile l'età del passeggero
+const numeroEta = eta;
+
+// 5. il prezzo del biglietto sarà definito in base ai km 0.21
+const prezzoalKm = '0.12';
+
+// 6. calcolo prezzo biglietto
+const prezzoBiglietto = numeroChilometri * prezzoalKm;
+
+// 7. istruzione condizionale
 //  -SE IL PASSEGGERO HA MENO DI 18 ANNI SCONTO DEL 20%
 //  -ALTRIMENTI SE IL PASSEGERO HA PIU DI 65 ANNI SCONTO DEL 40%
 //  -ALTRIMENTI IL PREZZO NON SARA SCONTATO
+
+const calcoloSconto20 = (prezzoBiglietto * 20) / 100;
+
+const sconto20 = prezzoBiglietto - calcoloSconto20;
+
+const calcoloSconto40 = (prezzoBiglietto * 40) / 100;
+
+const sconto40 = prezzoBiglietto - calcoloSconto40;
+
+if (numeroEta < '18') {
+  prezzoBiglietto = sconto20;
+} else if (numeroEta > '65') {
+  prezzoBiglietto = sconto40;
+} else numeroEta > '18' < '65';
+prezzoBiglietto;

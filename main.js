963dabf2341e378eb67403afcfb9451a1edf2d56
FilippoBersan <filippobersan99@gmail.com>
2023-11-06 14:42:33 +0100

    feat: scaffolding

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+// Calcolo del prezzo del biglieggto del treno
+
+// 1. mostro un prompt per il numero chilometri da percorrere
+// 2. mostro un prompt per l'età del passeggero
+// 3. il prezzo del biglietto sarà definito in base ai km 0.21
+
+// 4. istruzione condizionale
+//  -SE IL PASSEGGERO HA MENO DI 18 ANNI SCONTO DEL 20%
+//  -ALTRIMENTI SE IL PASSEGERO HA PIU DI 65 ANNI SCONTO DEL 40%
+//  -ALTRIMENTI IL PREZZO NON SARA SCONTATO
